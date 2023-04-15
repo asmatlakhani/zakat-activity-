6212c86c5614f65d3ceac73590c750e09d029638
asmat ullah <asmatlakhani@gmail.com>
2023-04-15 21:04:55 +0500

    change

--- app.js
+++
@@ -1,6 +0,0 @@
-
-// // task 1 zakat.....
-// var zakatpercentage = 0.025
-// var userinput = +promt("writeyour zakat contity");
-// var result=(zakatpercentage*userinput);
-// alert("your zakat value is"+result);
